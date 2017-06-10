fd162dac4a9b4d10137c39cc87074db0b3458114
TheFonix <actualfriction@gmail.com>
2017-06-10 13:02:05 +0100

    Got rid of the Username Section to free up space

--- resources/themes/pterodactyl/base/index.blade.php
+++ resources/themes/pterodactyl/base/index.blade.php
@@ -52,7 +52,6 @@
                 <table class="table table-hover">
                     <tbody>
                         <tr>
-                            <th>@lang('strings.username')</th>
                             <th>@lang('strings.name')</th>
                             <th>@lang('strings.node')</th>
                             <th>@lang('strings.connection')</th>
@@ -62,7 +61,6 @@
                         </tr>
                         @foreach($servers as $server)
                             <tr class="dynamic-update" data-server="{{ $server->uuidShort }}">
-                                <td @if(! empty($server->description)) rowspan="2" @endif><code>{{ $server->username }}</code></td>
                                 <td><a href="{{ route('server.index', $server->uuidShort) }}">{{ $server->name }}</a></td>
                                 <td>{{ $server->node->name }}</td>
                                 <td><code>{{ $server->allocation->alias }}:{{ $server->allocation->port }}</code></td>
