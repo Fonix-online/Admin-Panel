a0de07c80e3b98a401ca49fc927c90339755dc70
TheFonix <actualfriction@gmail.com>
2017-06-12 23:16:54 +0100

    Change of logo

--- resources/themes/pterodactyl/layouts/master.blade.php
+++ resources/themes/pterodactyl/layouts/master.blade.php
@@ -58,7 +58,7 @@
         <div class="wrapper">
             <header class="main-header">
                 <a href="{{ route('index') }}" class="logo">
-                     <img src="https://i.gyazo.com/959c754207cd40f0c3e04b9775af0904.png" alt="Smiley face" height="36" width="160">
+                     <img src="https://secure.ume.digitalhazards.net/p-logo/SSL-Logo-Ori.png" alt="Smiley face" height="36" width="160">
                 </a>
                 <nav class="navbar navbar-static-top">
                     <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
