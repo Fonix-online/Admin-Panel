268c3cbb5c60f9270771786aa484e8a2ab86e181
TheFonix <actualfriction@gmail.com>
2017-06-12 23:22:48 +0100

    Change Email

--- resources/themes/pterodactyl/layouts/master.blade.php
+++ resources/themes/pterodactyl/layouts/master.blade.php
@@ -79,7 +79,7 @@
                                 <a href="#" data-action="control-sidebar" data-toggle="tooltip" data-placement="bottom" title="{{ @trans('strings.servers') }}"><i class="fa fa-server"></i></a>
                             </li>
                             <li>
-                                <a href="mailto:surpport@iskall85.com" data-toggle="tooltip" data-placement="bottom" title="Email Surpport"><i class="fa fa-envelope"></i></a>
+                                <a href="mailto:self@digitalhazards.net" data-toggle="tooltip" data-placement="bottom" title="Email the Admin"><i class="fa fa-envelope"></i></a>
                             </li>
                             @if(Auth::user()->isRootAdmin())
                                 <li>
