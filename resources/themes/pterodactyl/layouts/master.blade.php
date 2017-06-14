e4587b03fd68aeac570008b92926c211fcd37754
Fonix Hardcastle <actualfriction@gmail.com>
2017-06-14 16:16:52 +0100

    Got sick of all the fuss and added an external Favicon!

--- resources/themes/pterodactyl/layouts/master.blade.php
+++ resources/themes/pterodactyl/layouts/master.blade.php
@@ -31,9 +31,9 @@
         <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
         <link rel="manifest" href="/favicons/manifest.json">
         <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#bc6e3c">
-        <link href="/img/favicon.ico?29452" rel="icon">
+        <link href="https://digitalhazards.net/assets/favicon/favicon.ico?29452" rel="icon">
         <meta name="msapplication-config" content="/favicons/browserconfig.xml">
-        <meta name="theme-color" content="#A4FD0B">
+        <meta name="theme-color" content="#367fa9">
 
         @include('layouts.scripts')
 
